4c4e76488cb45afc2dbd803ec2c853cc18ab9d33
Suyash Jain <suyashj@uci.edu>
2025-01-22 16:15:41 -0800

    No default dict said, so we removed it and also added time complexity or most of things

--- partA.py
+++ partA.py
@@ -1,39 +1,53 @@
 import sys
 import re
-from collections import defaultdict
 Token=""
 
 def tokenize(text_file_path: str) -> list[Token]:
-    tokens = []
-    regex_pattern = re.compile(r"[A-Za-z0-9]+")
+    ''' This function tokenises the file items and add em to the list.
+        we also use the open fn to handel the case of automatically closing the file, also enforcing uts-8 encoding.
+        I used 2 exceptions one in case file is not found/ name is wrong, and non-unicode text is read.
+        I also used a simple regex statement finding letters like (a-z), (A-Z), (0-9), (+, repetition operator)
+        Here Time complexity appears to be o(n+m), where n is total text length and m is number of lines, but since
+        we can't have more tokens than characters, we can say Time complexity is o(n).
+    '''
+    tokens = [] # o(1)
+    regex_pattern = re.compile(r"[A-Za-z0-9]+") # o(1)
     try:
-        with open(text_file_path, "r", encoding="utf-8") as f:
-            for line in f:
-                match = regex_pattern.findall(line)
-                for i in match:
+        with open(text_file_path, "r", encoding="utf-8") as f: # o(1)
+            for line in f: # o(m)
+                match = regex_pattern.findall(line) # o(n)
+                for i in match: # o(token)
                     tokens.append(i.lower())
-        return tokens
-    except FileNotFoundError:
-        print("Error: File was not found.")
+        return tokens # o(1)
+    except FileNotFoundError:# o(1)
+        print("Error: File was not found.") # o(1)
         sys.exit(1)
-    except UnicodeDecodeError:
-        print("Error: Not Unicode file.")
+    except UnicodeDecodeError:# o(1)
+        print("Error: Not Unicode file.") # o(1)
         sys.exit(1)
 
 
 def compute_word_frequencies(tokens: list[Token]) -> dict[Token, int]:
-    token_dict = {}
-    for i in tokens:
-        if i not in token_dict:
-            token_dict[i] = 0
-        token_dict[i] += 1
-    return token_dict
+    '''This function uses the previously computed tokenised list and add em to a map/dict.
+        Here Time complexity is o(n), where n is no. of tokens in the list, and we run it n times
+        to add tokens in the dict.
+    '''
+    token_dict = {} # o(1)
+    for i in tokens: # o(n)
+        if i not in token_dict: # o(1)
+            token_dict[i] = 0 # o(1)
+        token_dict[i] += 1 # o(1)
+    return token_dict # o(1)
 
 def print_frequencies(frequencies: dict[Token, int]) -> None:
-    sorted_dict = sorted(frequencies.items(), key=lambda item: item[1], reverse=True)
+    ''' This function prints the sorted token in a  format "i -> j", we sort using the sorted function
+        that has the time complexity of o(nlog(n)) and dominates the function. Hence, the time complexity of
+        the function is o(nlog(n)). Here n is items in the dictionary
+    '''
+    sorted_dict = sorted(frequencies.items(), key=lambda item: item[1], reverse=True) # o(nlog(n))
     # print(sorted_dict)
-    for i, j in sorted_dict:
-        print(f"{i} {j}")
+    for i, j in sorted_dict:  # o(n)
+        print(f"{i} -> {j}") # o(1)
 
 
 def main():
--- partB.py
+++ partB.py
@@ -2,19 +2,25 @@ import sys
 import partA
 
 def main():
+    '''
+        time complexity of this function is :o(n1+n2)log(n1+n2)
+        n1 =  no. of character in file 1, n2 =  no. of character in file 1,
+        since we are kind of doing sorted for common token, it should be o(n1+n2) + o(n1+n2)log(n1+n2)
+        which simplifies to be just : o(n1+n2)log(n1+n2)
+    '''
     if len(sys.argv) != 3:
         print("Error: Wrong Argument")
         sys.exit(1)
-    tokens1 = partA.tokenize(sys.argv[1])
-    tokens2 = partA.tokenize(sys.argv[2])
+    tokens1 = partA.tokenize(sys.argv[1]) # o(n1)
+    tokens2 = partA.tokenize(sys.argv[2]) # o(n2)
 
-    set1 = set(tokens1)
-    set2 = set(tokens2)
-    common_tokens = set1.intersection(set2)
+    set1 = set(tokens1) # o(n1)
+    set2 = set(tokens2) # o(n2)
+    common_tokens = set1.intersection(set2) # o(n1+n2)
 
-    print(f"No. of common tokens: {len(common_tokens)}")
-    for token in sorted(common_tokens):
-         print(token)
+    print(f"No. of common tokens: {len(common_tokens)}") # o(1)
+    for token in sorted(common_tokens): # sorted is o(log(n)), so here it becomes : o(n1+n2)log(n1+n2)
+         print(token) # o(1)
 
 
 if __name__ == "__main__":
